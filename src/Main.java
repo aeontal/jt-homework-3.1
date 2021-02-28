a204c91871185bf43955228090423bde328d5e63
Maxim Kazantsev <aeontal@gmail.com>
2021-02-28 17:42:48 +0500

    Реформат кода

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    public static void main(String[] args)
-    {   double amount = 1_075;
-        double mult=20;
-        double reminder=0;
-        double bonus_miles =reminder + amount / mult;
+    public static void main(String[] args) {
+        double amount = 1_075;
+        double mult = 20;
+        double reminder = 0;
+        double bonus_miles = reminder + amount / mult;
         double total_bonus = Math.floor(bonus_miles);
-        reminder=bonus_miles-total_bonus;
+        reminder = bonus_miles - total_bonus;
         System.out.println(total_bonus);
     }
 
